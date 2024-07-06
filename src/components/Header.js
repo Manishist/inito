ecb7a934fed465bbc0a24ef98452392ff06924fc
Manish <manishsonar347@gmail.com>
2024-07-06 05:52:46 +0530

    js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import './Header.css';
 
 const Header = () => {
+    const [navOpen, setNavOpen] = useState(false);
+
+    const toggleNav = () => {
+        setNavOpen(!navOpen);
+    };
+
     return (
         <header >
             <div className="logo">
                 <img src="https://dqxth8lmt6m4r.cloudfront.net/assets/v1/Logo-1b7ad53fafc81e97cf23af4f0ffae8d8961ee793cd3d982c132b3acfa8b75bdd.svg" alt="Logo"></img>
             </div>
             <nav className="navbar">
-                <ul className="nav-links">
+                <ul className={`nav-links ${navOpen ? 'flex' : 'dNone'} `}>
                     <li><a href="#">About Us</a></li>
                     <li><a href="#">Blog</a></li>
                     <li><a href="#">FAQ</a></li>
@@ -16,7 +22,7 @@ const Header = () => {
                     <li><a href="#">Careers</a></li>
                 </ul>
                 <button className="initoButton">Try Inito</button>
-                <div className="hamburger">
+                <div className="hamburger" onClick={toggleNav}>
                     <span></span>
                     <span></span>
                     <span></span>
